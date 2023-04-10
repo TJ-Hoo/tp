a970e538158ffb738dffd6e09827e3cd622337db
TJ\user <e0773494@u.nus.edu>
2023-04-10 11:49:08 +0800

    Nutrition Command Test

--- src/test/java/seedu/commands/NutritionCommandTest.java
+++ src/test/java/seedu/commands/NutritionCommandTest.java
@@ -10,17 +10,16 @@ import seedu.ui.GeneralUi;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 public class NutritionCommandTest {
-	private NutritionCommand nutritionCommand;
-	private GeneralUi ui = new GeneralUi();
-	private final ExerciseStorage exerciseStorage = new ExerciseStorage("./data/exerciseData.csv");
-	private final FoodStorage foodStorage = new FoodStorage();
-	private final MealStorage mealStorage = new MealStorage("./data/mealData.csv", foodStorage);
-	private final UserStorage userStorage = new UserStorage("./data/userData.csv");
+    private NutritionCommand nutritionCommand;
+    private GeneralUi ui = new GeneralUi();
+    private final ExerciseStorage exerciseStorage = new ExerciseStorage("./data/exerciseData.csv");
+    private final FoodStorage foodStorage = new FoodStorage();
+    private final MealStorage mealStorage = new MealStorage("./data/mealData.csv", foodStorage);
+    private final UserStorage userStorage = new UserStorage("./data/userData.csv");
 
-	@Test
-	public void nutrition_validInput_expectNoExceptionsThrown() throws LifeTrackerException {
-		nutritionCommand = new NutritionCommand();
-		assertDoesNotThrow(() -> nutritionCommand.execute(ui, foodStorage, mealStorage, userStorage, exerciseStorage));
-	}
-
+    @Test
+    public void nutrition_validInput_expectNoExceptionsThrown() throws LifeTrackerException {
+        nutritionCommand = new NutritionCommand();
+        assertDoesNotThrow(() -> nutritionCommand.execute(ui, foodStorage, mealStorage, userStorage, exerciseStorage));
+    }
 }
